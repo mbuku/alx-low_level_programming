63917571ab3c27b31ad35c7e1e870f6a1d112e2d
mbuku <joysmbuku@gmail.com>
2022-10-13 15:31:03 -0700

    M -files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,11 @@
-
 #include <stdio.h>
-
 /**
-
- * Description: main - Prints out a string to stdout.
-
- * Return: 0 if success.
-
+ * main - Entry
+ * Return: Always 0
  */
 
 int main(void)
-
 {
-
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
-
 }
