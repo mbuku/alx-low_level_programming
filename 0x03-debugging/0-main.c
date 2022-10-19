af82666075ac82e1c4a5ae8896c7ac291de27ce0
mbuku <joysmbuku@gmail.com>
2022-10-19 08:47:12 -0700

    M -file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * main - Test function for positive or negative
+  * Return: 0
+  */
+
+int main(void)
+
+{
+
+main.h int i;
+
+main.h i = 0;
+
+main.h positive_or_negative(i);
+
+return (0);
+
+}
